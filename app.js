38520771d014975222122abcdf38ab2920772390
Pat Dave <pat@dave.com>
2020-02-20 21:20:55 +0800

    added the cart page

--- app.js
+++ app.js
@@ -3,37 +3,37 @@ let carts = document.querySelectorAll('.add-cart')
 let products = [
     {
         name: 'Dark Classic Hoddie',
-        tag: 'DarkClassicHoddie',
+        tag: '01',
         price: 20,
         inCart: 0
     },
     {
         name: 'Metal Grey v2',
-        tag: 'MetalGreyv2',
+        tag: '02',
         price: 15,
         inCart: 0
     },
     {
         name: 'Light Grey no.7',
-        tag: 'LightGreyno.7',
+        tag: '03',
         price: 30,
         inCart: 0
     },
     {
         name: 'Sports Hoodie Fit',
-        tag: 'SportsHoodieFit',
+        tag: '04',
         price: 33,
         inCart: 0
     },
     {
         name: 'Blue Koala Hoodie',
-        tag: 'BlueKoalaHoodie',
+        tag: '05',
         price: 50,
         inCart: 0
     },
     {
         name: 'Gray Bare Bear Hoodie',
-        tag: 'GrayBareBearHoodie',
+        tag: '06',
         price: 40,
         inCart: 0
     }
@@ -109,7 +109,51 @@ function setItems(product) {
         }
     }
 
+    function displayCart() {
+        let cartItems = localStorage.getItem("productsInCart")
+        cartItems = JSON.parse(cartItems)
+        let productContainer = document.querySelector(".products")
+        let cartCost = localStorage.getItem('totalCost')
+    
+        console.log(cartItems)
+
+        if (cartItems && productContainer) {
+            productContainer.innerHTML = ''
+            Object.values(cartItems).map(item => {
+                productContainer.innerHTML += `
+                <div class="product">
+                    <ion-icon name="close-circle"></ion-icon>
+                    <img src="./img/${item.tag}.jpg" 
+                    style="height: 120px;
+                    width: 100px;">
+                    <span>${item.name}</span>
+                </div>
+                
+                <div class="price">$${item.price},00</div>
+                <div class="quantity">
+                <ion-icon name="caret-back-circle-sharp"></ion-icon>
+                    <span>${item.inCart}</span>
+                <ion-icon name="caret-forward-circle-sharp"></ion-icon>
+                </div>
+                <div class="total">
+                    $${item.inCart * item.price},00
+                </div>
+                `
+            })
+
+            productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                     Basket Total
+                </h4>
+                <h4 class="basketTotal">
+                    $${cartCost},00
+                </h4>
+                </div>
+                `
+        }
+    }
 
 
 onLoadCartNumbers()
-
+displayCart()
